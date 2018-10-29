053368178c923d4872dc1b91e7b500076971c71a
Matthieu Marinangeli <matthieu.marinangeli@epfl.ch>
2018-10-29 17:58:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(  name = "statnight",
 	test_suite = "tests",
 	install_requires = ir,
 	setup_requires = ["pytest-runner"],
-	tests_require = ,
+	tests_require = tr,
 	classifiers = [
 			"Intended Audience :: Science/Research",
 			"Operating System :: MacOS",
