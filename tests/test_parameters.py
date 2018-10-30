4ee93737ba2f79d3dca8705247e09273303a9206
Matthieu Marinangeli <matthieu.marinangeli@epfl.ch>
2018-10-30 14:09:47 +0100

    remove Object from test_parameters.py

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
 import pytest
 
-from statnight.parameters import Object, Named, Range, Observable, Variable, Constant
+from statnight.parameters import Named, Range, Observable, Variable, Constant
 
-def test_constructors():	
-	o = Object()
-	
+def test_constructors():
+
 	n = Named(name="n")
 	with pytest.raises(TypeError):
 		Named()
-		
+
 	r = Range(range=(1,2))
 	with pytest.raises(TypeError):
 		Range()
@@ -23,7 +22,7 @@ def test_constructors():
 		Range(range=[1,"2"])
 	with pytest.raises(ValueError):
 			Range(range=[1,-1])
-		
+
 	obs = Observable("obs", (1,2))
 	with pytest.raises(TypeError):
 		Observable("obs")
@@ -33,13 +32,13 @@ def test_constructors():
 		Observable("obs", [1,"2"])
 	with pytest.raises(ValueError):
 		Observable("obs",range=[1,-1])
-	
+
 	const = Constant("const", 1.4)
 	with pytest.raises(TypeError):
 		Constant("const")
 	with pytest.raises(TypeError):
 		Constant("const", "3")
-	
+
 	var = Variable("var", (1,2))
 	with pytest.raises(TypeError):
 		Variable("var")
@@ -65,66 +64,66 @@ def test_constructors():
 		def f(a):
 			return str(a)
 		Variable("var", range=(1,2), constraint=f)
-		
+
 def test_properties():
-	
+
 	n = Named(name="n")
 	assert n.name == "n"
-	
+
 	r = Range(range=(1,2))
 	assert r.range == (1,2)
-	
+
 	obs = Observable("obs", (1,2))
 	assert obs.name == "obs"
 	assert obs.range == (1,2)
-	
+
 	const = Constant("const", 1.4)
 	assert const.name == "const"
 	assert const.value == 1.4
-			
+
 	var = Variable("var", (1,2))
 	assert var.name == "var"
 	assert var.range == (1,2)
 	assert var.initvalue == 1.5
 	assert var.initstep  == 0.01
 	assert var.constraint == None
-	
+
 	def f(a):
 		return a+2
-		
+
 	var = Variable("var", (1,2), initvalue=1.3, initstep=0.3, constraint=f)
 	assert var.name == "var"
 	assert var.range == (1,2)
 	assert var.initvalue == 1.3
 	assert var.initstep  == 0.3
 	assert var.constraint == f
-			
+
 def test_methods():
-	
+
 	const = Constant("const", 1.4)
 	minuit_const = const.tominuit()
 	assert len(minuit_const.keys()) == 2
 	assert minuit_const["const"] == 1.4
 	assert minuit_const["fix_const"] == True
-	
+
 	var = Variable("var", (1,2))
 	minuit_var = var.tominuit()
 	assert len(minuit_var.keys()) == 3
 	assert minuit_var["var"] == var.initvalue
 	assert minuit_var["limit_var"] == var.range
 	assert minuit_var["error_var"] == var.initstep
-	
+
 def test_repr():
-	
+
 	obs = Observable("obs", (1,2))
 	assert repr(obs) == "Observable('obs', range=(1, 2))"
-	
+
 	const = Constant("const", 1.4)
 	assert repr(const) == "Constant('const', value=1.4)"
-	
+
 	var = Variable("var", (1,2))
 	assert repr(var) == "Variable('var', initvalue=1.5, range=(1, 2), initstep=0.01)"
-	
+
 	def f(a):
 		return (a-2)**2
 	var = Variable("var", (1,2), initvalue=1.2, initstep=0.1, constraint=f)
